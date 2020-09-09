d6f743cb4451f3b7aa149c1ba7957d8cb6fc1f85
Tim Schumacher <tim@datenknoten.me>
2020-09-09 13:04:21 +0200

    fix: ditch small package

--- tools/install.psm1
+++ tools/install.psm1
@@ -5,26 +5,27 @@ $minorVersion = '1'
 
 $emacsNameVersion = "$($packageName)-$($majorVersion).$($minorVersion)"
 $installDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition) + '/' + $packageName
+$baseUrl = "https://ftp.gnu.org/pub/gnu/emacs/windows"
 
 $FullPackageArgs = @{
     packageName    = $packageName
     unzipLocation  = $installDir
-    url            = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$($emacsNameVersion)-i686.zip"
-    url64bit       = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$($emacsNameVersion)-x86_64.zip"
-    checksum       = 'bd99eae036b229e6105c070499c63994870c839c70404a12c04992e0c0fa0b5b'
+    url            = "$($baseUrl)/emacs-$($majorVersion)/$($emacsNameVersion)-i686.zip"
+    url64bit       = "$($baseUrl)/emacs-$($majorVersion)/$($emacsNameVersion)-x86_64.zip"
+    checksum       = 'a52a1126825f3bf02b727f727874bfccb82ffaab4e914defbeaec28eb3ed6f1e'
     checksumType   = 'sha256'
-    checksum64     = '3b0b2e9355fccd07f67bc7272cabeaa6ae82f8f3cbe54008b611a331f61b4c7d'
+    checksum64     = '65f1b01bcd14e59d7da0ebad3979c5df9f9bd8d24b2ff2e32a5af3aacb226229'
     checksumType64 = 'sha256'
 }
 
 $NoDepsPackageArgs = @{
     packageName    = $packageName
     unzipLocation  = $installDir
-    url            = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$($emacsNameVersion)-i686-no-deps.zip"
-    url64bit       = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$($emacsNameVersion)-x86_64-no-deps.zip"
-    checksum       = 'd9a5d3513f8aad075068db2f41a08676f50db900b62ce32c6edd54cbc7c54392'
+    url            = "$($baseUrl)/emacs-$($majorVersion)/$($emacsNameVersion)-i686-no-deps.zip"
+    url64bit       = "$($baseUrl)/emacs-$($majorVersion)/$($emacsNameVersion)-x86_64-no-deps.zip"
+    checksum       = '5758d2baa6c78364042b44ab15b7a78b172eed51fa9863416e17e123788e5094'
     checksumType   = 'sha256'
-    checksum64     = 'dc00a3bd152d984f079472d2fb34af6ae4beb9bfef56162d6aa4a6685571900a'
+    checksum64     = 'ccdaa194d80b6d5c7d087267e2e57211eb98012fa135c917f1ba714d09e7f641'
     checksumType64 = 'sha256'
 }
 
@@ -33,8 +34,8 @@ $emacsDepsVersion = "emacs-$($majorVersion)"
 $DepsPackageArgs = @{
   packageName    = $packageName
   unzipLocation  = $installDir
-  url            = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$($emacsDepsVersion)-i686-deps.zip"
-  url64bit       = "https://alpha.gnu.org/gnu/emacs/pretest/windows/emacs-$($majorVersion)/$($emacsDepsVersion)-x86_64-deps.zip"
+  url            = "$($baseUrl)/emacs-$($majorVersion)/$($emacsDepsVersion)-i686-deps.zip"
+  url64bit       = "$($baseUrl)/emacs-$($majorVersion)/$($emacsDepsVersion)-x86_64-deps.zip"
   checksum       = '1F28BEA4A14E86817B730C7ED71905407436B98B5ABCC1BDFE32513B74311C7C'
   checksumType   = 'sha256'
   checksum64     = '9597cccaf3eb8a26eb5504327a042354d11760b7aae5e83c75ce793491d2de1d'
